d70862271f2bc38638219b0665aeca6cd235689c
Waldo1628 <41935443+Waldo1628@users.noreply.github.com>
2022-03-15 16:31:17 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,60 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, KeyboardAvoidingView, Platform } from 'react-native';
+import { Provider } from "react-redux";
+import HomeScreen from './screens/HomeScreen';
+import MapScreen from './screens/MapScreen';
+import { store } from './store';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import 'react-native-gesture-handler'
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { LogBox } from 'react-native';
+
+
+
+//Set up redux
+
+export default function App() {
+
+const Stack = createNativeStackNavigator();
+
+  return (
+    <Provider store={store}>
+      <NavigationContainer>
+      <SafeAreaProvider>
+        <KeyboardAvoidingView 
+          behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={{flex: 1}}
+          keyboardVerticalOffset={Platform.OS === "ios" ? -64 : 0}
+        >
+        <Stack.Navigator>
+          <Stack.Screen
+            name = 'HomeScreen'
+            component={HomeScreen}
+            options={{
+              headerShown: false,
+
+
+            }}
+          />
+          <Stack.Screen
+            name = 'MapScreen'
+            component={MapScreen}
+            options={{
+              headerShown: false,
+              
+
+            }}
+          />
+        </Stack.Navigator>
+        </KeyboardAvoidingView>
+        
+      </SafeAreaProvider>
+      </NavigationContainer>
+    </Provider>
+    
+  );
+}
+LogBox.ignoreLogs([
+  "[react-native-gesture-handler] Seems like you\'re using an old API with gesture components, check out new Gestures system!",
+]);
